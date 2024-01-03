dde5575c0c9170e39ed1b5c5b47651aa059b39a7
AnnYersh <yershidze@gmail.com>
2024-01-03 19:44:05 +0500

    js42

--- app.js
+++ app.js
@@ -4,23 +4,9 @@ let nom = prompt('Введите Ваше имя')
 let year = +prompt('Введите текущий год')
 let birth = +prompt('Введите год Вашего рождения')
 
-function age(old,you) {
-    let count = Math.abs(you - old) 
+function age(bilo,stalo) {
+    let count = stalo - bilo
     return count
 }
 
-console.log('Вас зовут ' + nom + ', Ваш возраст ' + age(year,birth))
-
-
-/* ! Тут то же самое, но можно ли написать короче? Строка 22*/
-
-// let nom = prompt('Введите Ваше имя')
-// let year = +prompt('Введите текущий год')
-// let birth = +prompt('Введите год Вашего рождения')
-
-// function age(old,you) {
-//     return count = Math.abs(you - old) 
-// }
-
-// console.log('Вас зовут ' + nom + ', Ваш возраст ' + age(year,birth))
-
+console.log('Вас зовут ' + nom + ', Ваш возраст ' + age(birth,year))~
