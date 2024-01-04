e76da23ff11cb32248ae676b53160abd55f909af
AnnYersh <yershidze@gmail.com>
2024-01-04 17:17:17 +0500

    js44

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 // /* Задача №1 */
 
-// let nom = prompt('Введите Ваше имя')
-// let year = +prompt('Введите текущий год')
-// let birth = +prompt('Введите год Вашего рождения')
+let nom = prompt('Введите Ваше имя')
+let year = +prompt('Введите текущий год')
+let birth = +prompt('Введите год Вашего рождения')
 
-// function age(bilo,stalo) {
-//     let count = stalo - bilo
-//     return count
-// }
+function age(bilo,stalo) {
+    let count = stalo - bilo
+    return count
+}
 
-// console.log('Вас зовут ' + nom + ', Ваш возраст ' + age(birth,year))
+console.log('Вас зовут ' + nom + ', Ваш возраст ' + age(birth,year))
 
 //Задача 2
 
